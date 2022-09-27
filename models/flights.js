37be6cc4431ea5c17bc09e392539fd162e297425
Jenn <jenniferjuarezjj@gmail.com>
2022-09-27 08:13:53 -0400

    flight properties done, step 3

--- models/flights.js
+++ models/flights.js
@@ -5,6 +5,24 @@ const Schema = mongoose.Schema
 
 
 const flightSchema = new Schema({
-  
-
+  airline: {
+    type: String,
+    enum:['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: 'DEN'
+  },
+  FlightNo: {
+    type: Number,
+    min: 10,
+    max: 9999,
+  },
+  departs: {
+    type: Date,
+    default: function() {
+      return new Date().setFullYear(new Date().getFullYear() + 1)
+    },
+  },
 })
