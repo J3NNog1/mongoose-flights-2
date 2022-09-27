26fe474da63492e9b98f0bf67d9c5f55028f3707
Jenn <jenniferjuarezjj@gmail.com>
2022-09-27 19:28:44 -0400

    create new flights and it shows up in Azure workspace

--- controllers/flights.js
+++ controllers/flights.js
@@ -6,9 +6,27 @@ function newFlight(req, res) {
   res.render('flights/new', {
     title: 'All Flights'
   })
-
 }
+function create(req, res) {
+// convert nowShowing's checkbox of nothing or "on" to boolean
+  req.body.nowShowing = !!req.body.nowShowing
+// replace and split if it's not an empty string
+  if (req.body.cast) {
+  // remove whitespace next to commas
+  req.body.cast = req.body.cast.split(', ')
+  }
+  Flight.create(req.body)
+  .then(flight => { 
+    console.log(flight)
+    res.redirect(`/flights/new`)
+  })
+  .catch(err => {
+    res.redirect('/flights/new')
+  })
+}
+
 
 export {
   newFlight as new,
+  create,
 }
--- routes/flight.js
+++ routes/flight.js
@@ -5,6 +5,8 @@ import * as flightsCtrl from '../controllers/flights.js'
 // GET /flights/new
 router.get('/new', flightsCtrl.new)
 
+//POST  /flights
+router.post('/', flightsCtrl.create)
 export {
 	router
 }
